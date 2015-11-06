61eb2c63e74b985e6143300bea0a50f1e9179bec
awafa13 <awafa@globalrescue.com>
2015-11-06 22:23:12 +0500

    commit

---
+++ GRID/src/defaultpackage/test.java
@@ -0,0 +1,28 @@
+package defaultpackage;
+
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.JavascriptExecutor;
+
+public class test {
+	public WebDriver driver;
+  @Test
+  public void f() {
+	  ((JavascriptExecutor) driver).executeScript("document.getElementsByName('signUpForm:j_idt77')[0].click()");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new FirefoxDriver();
+	  driver.get("http://pkgridtst1/grcom/signup/signup.xhtml");
+	  
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
